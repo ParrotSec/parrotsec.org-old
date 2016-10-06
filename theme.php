ffc733f03280f157cc56888df2ee624627d7f769
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-10-06 18:17:25 +0200

    rename mdl-demo in mdl-parrot

--- theme.php
+++ theme.php
@@ -4,7 +4,7 @@
 			include("head.php");
 		?>
 	</head>
-	<body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
+	<body class="mdl-parrot mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
 		<?php
 			include("body.php");
 		?>
