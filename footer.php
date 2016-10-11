3676d89714274465599ae81cac6bcff0a19188de
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-10-11 21:15:37 +0200

    ultimate last changes

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
             <div class="mdl-mega-footer--middle-section">
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Features</h1>
+                    <h1 class="mdl-mega-footer--heading">Info</h1>
                     <ul class="mdl-mega-footer--link-list">
                         <li><a href="https://docs.parrotsec.org/what-is-parrot">About</a></li>
                         <li><a href="https://docs.parrotsec.org/terms-and-conditions">Terms</a></li>
@@ -12,42 +12,38 @@
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Details</h1>
+                    <h1 class="mdl-mega-footer--heading">Talking about us</h1>
                     <ul class="mdl-mega-footer--link-list">
-                        <li><a href="#">Spec</a></li>
-                        <li><a href="#">Tools</a></li>
-                        <li><a href="#">Resources</a></li>
+                        <li><a target="_blank" href="https://en.wikipedia.org/wiki/Parrot_Security_OS">Wikipedia</a></li>
+                        <li><a target="_blank" href="http://distrowatch.com/table.php?distribution=parrotsecurity">Distrowatch</a></li>
+                        <li><a target="_blank" href="http://linux.softpedia.com/get/System/Operating-Systems/Linux-Distributions/Parrot-security-OS-102936.shtml">Softpedia</a></li>
                     </ul>
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">Technology</h1>
+                    <h1 class="mdl-mega-footer--heading">Community</h1>
                     <ul class="mdl-mega-footer--link-list">
-                        <li><a href="#">How it works</a></li>
-                        <li><a href="#">Patterns</a></li>
-                        <li><a href="#">Usage</a></li>
-                        <li><a href="#">Products</a></li>
-                        <li><a href="#">Contracts</a></li>
+                        <li><a target="_blank" href="http://chat.frozenbox.org:3989/?nick=parrot_....&channels=parrot&prompt=1">IRC</a></li>
+                        <li><a target="_blank" href="https://telegram.me/joinchat/AerigkC4pztMc7Hus7vRZg">Telegram</a></li>
+                        <li><a target="_blank" href="http://lists.parrotsec.org/listinfo">Mailing Lists</a></li>
+                        <li><a target="_blank" href="https://www.facebook.com/groups/parrotsec/">Facebook group</a></li>
                     </ul>
                 </div>
                 <div class="mdl-mega-footer--drop-down-section">
                     <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
-                    <h1 class="mdl-mega-footer--heading">FAQ</h1>
+                    <h1 class="mdl-mega-footer--heading">Social Pages</h1>
                     <ul class="mdl-mega-footer--link-list">
-                        <li><a href="#">Questions</a></li>
-                        <li><a href="#">Answers</a></li>
-                        <li><a href="#">Contact us</a></li>
+                        <li><a target="_blank" href="https://github.com/parrotsec">GitHub</a></li>
+                        <li><a target="_blank" href="https://facebook.com/parrotsec">Facebook</a></li>
+                        <li><a target="_blank" href="https://twitter.com/parrotsec">Twitter</a></li>
+                        <li><a target="_blank" href="https://www.youtube.com/channel/UCj2dezzTc_Oy9eAEwBBodpw">Youtube</a></li>
                     </ul>
                 </div>
             </div>
             <div class="mdl-mega-footer--bottom-section">
                 <div class="mdl-logo">
-                    More Information
+                    Legal Notes
                 </div>
-                <ul class="mdl-mega-footer--link-list">
-                    <li><a href="https://developers.google.com/web/starter-kit/">Web Starter Kit</a></li>
-                    <li><a href="#">Help</a></li>
-                    <li><a href="#">Privacy and Terms</a></li>
-                </ul>
+                Parrot Security OS 2011-2016 &copy; <a href="https://docs.parrotsec.org/user/palinuro">Lorenzo Faletra</a> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="_blank">GNU/GPL v3</a>
             </div>
-        </footer>
+</footer>
