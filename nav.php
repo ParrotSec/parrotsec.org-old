c3e600b76ca22c23a7b2b2fcb6415b6c681435af
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-10-11 21:26:08 +0200

    fix nav links

--- nav.php
+++ nav.php
@@ -7,13 +7,13 @@
         <div class="mdl-layout--large-screen-only mdl-layout__header-row">
         </div>
         <div class="mdl-layout__tab-bar mdl-js-ripple-effect mdl-color--primary-dark">
-            <a href="https://www.parrotsec.org/" <?php if($page=="home"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab<?php } ?>>Home</a>
-            <a href="https://www.parrotsec.org/download.fx" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab<?php } ?>>Download</a>
-            <a href="https://www.parrotsec.org/features.fx" <?php if($page=="features"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab<?php } ?>>Features</a>
+            <a href="https://www.parrotsec.org/" <?php if($page=="home"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Home</a>
+            <a href="https://www.parrotsec.org/download.fx" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Download</a>
+            <a href="https://www.parrotsec.org/features.fx" <?php if($page=="features"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Features</a>
             <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">Blog</a>
             <a href="https://docs.parrotsec.org/" class="mdl-layout__tab">Documentation</a>
             <a href="https://docs.parrotsec.org/devel" class="mdl-layout__tab">Development</a>
-            <a href="https://docs.parrotsec.org/doku.php/community" class=""mdl-layout__tab">Community</a>
+            <a href="https://docs.parrotsec.org/doku.php/community" class="mdl-layout__tab">Community</a>
             <a href="https://docs.parrotsec.org/partners" class="mdl-layout__tab">Partners</a>
             <a href="https://docs.parrotsec.org/donate#gadgets" class="mdl-layout__tab">Gadgets</a>
             <a href="https://docs.parrotsec.org/FAQ" class="mdl-layout__tab">FAQ</a>
