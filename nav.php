ef3fc4d04bc02b5e58a0204bd791028a8f4d05a7
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2016-11-27 00:01:35 +0100

    change menu texts

--- nav.php
+++ nav.php
@@ -10,12 +10,12 @@
             <a href="https://www.parrotsec.org/" <?php if($page=="home"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Home</a>
             <a href="https://www.parrotsec.org/download.fx" <?php if($page=="download"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Download</a>
             <a href="https://www.parrotsec.org/features.fx" <?php if($page=="features"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Features</a>
-            <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">Blog</a>
+            <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">News</a>
             <a href="https://docs.parrotsec.org/" class="mdl-layout__tab">Documentation</a>
-            <a href="https://docs.parrotsec.org/devel" class="mdl-layout__tab">Development</a>
+            <a href="https://docs.parrotsec.org/devel" class="mdl-layout__tab">Devel</a>
             <a href="https://docs.parrotsec.org/doku.php/community" class="mdl-layout__tab">Community</a>
             <a href="https://docs.parrotsec.org/partners" class="mdl-layout__tab">Partners</a>
-            <a href="https://docs.parrotsec.org/donate#gadgets" class="mdl-layout__tab">Gadgets</a>
+            <a href="https://docs.parrotsec.org/donate#gadgets" class="mdl-layout__tab">Donations</a>
             <a href="https://docs.parrotsec.org/FAQ" class="mdl-layout__tab">FAQ</a>
             <a href="https://github.com/parrotsec/parrotsec.org" target="_blank" class="mdl-layout__tab">Edit</a>
         </div>
