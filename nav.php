c82507e8ec1aef3fc9793ba8c86a80e834b33fae
Lorenzo Faletra <palinuro@parrotsec.org>
2017-04-14 16:11:23 +0000

    Update menu links

--- nav.php
+++ nav.php
@@ -12,11 +12,11 @@
             <a href="https://www.parrotsec.org/features.fx" <?php if($page=="features"){?>class="mdl-layout__tab is-active"<?php } else { ?>class="mdl-layout__tab"<?php } ?>>Features</a>
             <a href="https://blog.parrotsec.org/" class="mdl-layout__tab">News</a>
             <a href="https://docs.parrotsec.org/" class="mdl-layout__tab">Documentation</a>
-            <a href="https://docs.parrotsec.org/devel" class="mdl-layout__tab">Devel</a>
+            <a href="https://dev.parrotsec.org" class="mdl-layout__tab">Devel</a>
             <a href="https://docs.parrotsec.org/doku.php/community" class="mdl-layout__tab">Community</a>
             <a href="https://docs.parrotsec.org/partners" class="mdl-layout__tab">Partners</a>
             <a href="https://docs.parrotsec.org/donate#gadgets" class="mdl-layout__tab">Donations</a>
             <a href="https://docs.parrotsec.org/FAQ" class="mdl-layout__tab">FAQ</a>
-            <a href="https://github.com/parrotsec/parrotsec.org" target="_blank" class="mdl-layout__tab">Edit</a>
+            <a href="https://dev.parrotsec.org/parrot/parrotsec.org" target="_blank" class="mdl-layout__tab">Edit</a>
         </div>
     </header>
