1396598072758586147bdcafc3495da66aedf148
DeaVulTan <deavultan@gmail.com>
2017-11-09 22:46:59 +0500

    update language detection system

--- include/lang.php
+++ include/lang.php
@@ -2,6 +2,10 @@
 
 $language = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
-include_once 'languages/'.core_.$language.'.php';
+ if(file_exists('languages/'.core_.$language.'.php')) {
+	include_once('languages/'.core_.$language.'.php');
+ }else{
+	include_once('languages/core_en.php');
+ }
 
 ?>
