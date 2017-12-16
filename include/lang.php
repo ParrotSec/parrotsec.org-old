27e7e6a87c029153a5f720507b6fb212101270da
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2017-12-16 02:46:08 +0100

    woops

--- include/lang.php
+++ include/lang.php
@@ -4,34 +4,34 @@
 $language = htmlentities(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
 
 #include default english language
-include_once('languages/core.en.php');
+include_once('languages/core_en.php');
 
 
 
 #override english strings with localized translations if available
 if ($language == "ar") {
-	include_once('languages/core.ar.php');
+	include_once('languages/core_ar.php');
 }
 elseif ($language == "de") {
-	include_once('languages/core.de.php');
+	include_once('languages/core_de.php');
 }
 elseif ($language == "fr") {
-	include_once('languages/core.fr.php');
+	include_once('languages/core_fr.php');
 }
 elseif ($language == "it") {
-	include_once('languages/core.it.php');
+	include_once('languages/core_it.php');
 }
 elseif ($language == "nl") {
-	include_once('languages/core.nl.php');
+	include_once('languages/core_nl.php');
 }
 elseif ($language == "ro") {
-	include_once('languages/core.ro.php');
+	include_once('languages/core_ro.php');
 }
 elseif ($language == "ru") {
-	include_once('languages/core.ru.php');
+	include_once('languages/core_ru.php');
 }
 elseif ($language == "uz") {
-	include_once('languages/core.uz.php');
+	include_once('languages/core_uz.php');
 }
 
 ?>
