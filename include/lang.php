0f79710eb9e714bf53258a3871957c82d1f1d2dd
Pino Matranga <pino.matranga1999@gmail.com>
2018-05-24 00:25:20 +0200

    Added extended lang name into dropdown

--- include/lang.php
+++ include/lang.php
@@ -1,11 +1,17 @@
 <?php
-$languages = array('it', 'en', 'ru', 'uz');
+$languages = array(
+    'it' => 'Italian',
+    'en' => 'English',
+    'ru' => 'Russian',
+    'uz' => 'Uzbek'
+);
 
 if(isset($_GET['lang'])){
     $language = $_GET['lang'];
 } else {
     $language = 'en';
 }
+
 $file_path = "languages/core_$language.php";
 if(file_exists($file_path))
     include_once($file_path);
--- include/layout.php
+++ include/layout.php
@@ -75,18 +75,17 @@
                       <a href="#" class="dropdown-toggle" type="button" id="languageDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><?php echo $lang['nav-16']; ?><span class="caret"></span></a>
                       <ul class="dropdown-menu language-navbar" aria-labelledby="languageDropdown">
                           <?php
-                            for($i=0; $i<count($languages); $i++) {
+                            foreach($languages as $key => $value) {
                                 $link = 'http://' . $_SERVER[HTTP_HOST] . $_SERVER[REQUEST_URI];
                                 if(!strpos($link, "?lang="))
-                                    $final_link = $link.'?lang=' . $languages[$i];
+                                    $final_link = "$link.?lang=$key";
                                 else {
                                     $language = $_GET['lang'];
                                     $search = "?lang=$language";
-                                    $replace = "?lang=$languages[$i]";
+                                    $replace = "?lang=$key";
                                     $final_link = str_replace($search, $replace, $link);
                                 }
-                                echo "<li><a href='$final_link' style='height: auto'><span style='color: #fff'>$languages[$i]</span><img src='img/lang/$languages[$i].ico' class='language-select pull-right'/></a></li>";
-//                                echo "<li><a href='$final_link'><span style='color: #fff; display: block; margin-left: auto; margin-right: auto;' class='pull-left'>$languages[$i]</span><img src='img/lang/$languages[$i].ico' class='pull-right langauge-select'/></a></li>";
+                                echo "<li><a href='$final_link' style='height: auto'><span style='color: #fff'>$value</span><img src='img/lang/$key.ico' class='language-select pull-right'/></a></li>";
                             }
                           ?>
                       </ul>
