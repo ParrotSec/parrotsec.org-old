020a224ac0569ab25c6d022841ffc78701a4af63
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2018-09-08 17:38:48 +0200

    update download links for parrot 4.2.1

--- download-home.php
+++ download-home.php
@@ -32,16 +32,16 @@
                   <li class="list-group-item"><i class="icon-ok text-info"></i><?php echo $lang['download-home-9'] . " " . PARROT_LITE_SIZE_64; ?></li>
     						</ul>
     						<div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_amd64.iso"><?php echo $lang['index-3']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_amd64.iso"><?php echo $lang['index-3']; ?></a>
     						</div>
                 <div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_amd64.iso.mirrorlist"><?php echo $lang['download-home-10']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_amd64.iso.mirrorlist"><?php echo $lang['download-home-10']; ?></a>
     						</div>
                 <div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_amd64.iso.torrent"><?php echo $lang['download-home-11']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_amd64.iso.torrent"><?php echo $lang['download-home-11']; ?></a>
     						</div>
                 <div class="panel-footer">
-                  <a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2/signed-hashes.txt"><?php echo $lang['download-home-12']; ?></a>
+                  <a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/signed-hashes.txt"><?php echo $lang['download-home-12']; ?></a>
     						</div>
     					</div>
             </div>
@@ -61,16 +61,16 @@
                   <li class="list-group-item"><i class="icon-ok text-info"></i><?php echo $lang['download-home-9'] . " " . PARROT_LITE_SIZE_32; ?></li>
     						</ul>
     						<div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_i386.iso"><?php echo $lang['index-3']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_i386.iso"><?php echo $lang['index-3']; ?></a>
     						</div>
                 <div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_i386.iso.mirrorlist"><?php echo $lang['download-home-10']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_i386.iso.mirrorlist"><?php echo $lang['download-home-10']; ?></a>
     						</div>
                 <div class="panel-footer">
-    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2/Parrot-home-4.2_i386.iso.torrent"><?php echo $lang['download-home-11']; ?></a>
+    							<a class="btn btn-lg btn-block btn-info" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/Parrot-home-4.2.1_i386.iso.torrent"><?php echo $lang['download-home-11']; ?></a>
     						</div>
                 <div class="panel-footer">
-                  <a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2/signed-hashes.txt"><?php echo $lang['download-home-12']; ?></a>
+                  <a class="btn btn-lg btn-block btn-info" target="_blank" href="https://cdimage.parrotsec.org/parrot/iso/4.2.1/signed-hashes.txt"><?php echo $lang['download-home-12']; ?></a>
     						</div>
     					</div>
             </div>
