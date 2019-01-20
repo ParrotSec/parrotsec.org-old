a9aa4508964ffdf1e570b372c7618c1587d66981
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-01-20 14:31:25 +0100

    change site name

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-const CONFIG_SITE_NAME = 'Parrot Project';
+const CONFIG_SITE_NAME = 'Parrot Linux';
 const CONFIG_SITE_SLOGAN = 'The best choice for security experts, developers and crypto-addicted people.';
 const CONFIG_SITE_DESCRIPTION = 'Discover our awesome cyber security GNU/Linux environment. It includes a full portable laboratory for security and digital forensics experts, but it also includes all you need to develop your own softwares or protect your privacy with anonymity and crypto tools.';
 const CONFIG_SITE_URL = 'https://www.parrotsec.org/';
