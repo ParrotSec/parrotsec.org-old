b567e82eb24831312c43a7e31c1753d4e6ed6001
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-07-17 00:44:41 +0200

    REVEEEEEEEEEEERT!

--- include/config.inc.php
+++ include/config.inc.php
@@ -7,7 +7,7 @@ const CONFIG_SITE_URL = 'https://www.parrotsec.org/';
 const CONFIG_SITE_URL_STATIC_RESOURCES = 'https://www.parrotsec.org/';
 const CONFIG_SITE_AUTOR = 'Lorenzo Faletra';
 const CONFIG_SITE_LANG = 'en';
-const CONFIG_SYSTEM_VERSION = '4.7';
+const CONFIG_SYSTEM_VERSION = '4.6';
 
 const PARROT_FULL_SIZE_64 = '~4GB';
 
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <hr>
                 <a href="download.php" class="btn btn-primary btn-xl"><?php echo $lang['index-3']; ?></a> &nbsp; <a href="https://parrotsec.org/docs/intro/what-is-parrot/" class="btn btn-primary btn-xl page-scroll"><?php echo $lang['index-4']; ?></a>
                 <br>
-                <h3> Parrot 4.7 released! <a href="https://parrotsec.org/download.php">Try it!</a>
+                <h3>Parrot 4.7 Beta is out! <a href="https://download.parrotsec.org/parrot/iso/4.7/">Try it!</a>
             </div>
         </div>
     </header>
