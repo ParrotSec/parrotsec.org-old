58a78cd069c98f06fd85c8ed6eb6842e42594d64
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-07-26 17:12:11 +0200

    fix homepage message

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <hr>
                 <a href="download.php" class="btn btn-primary btn-xl"><?php echo $lang['index-3']; ?></a> &nbsp; <a href="https://docs.parrot.sh/intro/what-is-parrot/" class="btn btn-primary btn-xl page-scroll"><?php echo $lang['index-4']; ?></a>
                 <br>
-                <h3> Parrot 4.7 released! <a href="https://www.parrot.sh/download.php">Try it!</a></h3>
+                <h3> Parrot 4.7 BETA released! <a href="https://download.parrot.sh/parrot/iso/4.7/">Try it!</a></h3>
                 <h3> We have migrated to the new <strong>parrot.sh</strong> domain</h3>
             </div>
         </div>
