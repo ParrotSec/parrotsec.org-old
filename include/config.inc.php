e88673580d85b3f5c47d8e27725a193e9a6b2424
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-08-10 21:53:30 +0200

    update reference links and site description

--- include/config.inc.php
+++ include/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 const CONFIG_SITE_NAME = 'Parrot OS';
-const CONFIG_SITE_SLOGAN = 'The best choice for security experts, developers and crypto-addicted people.';
+const CONFIG_SITE_SLOGAN = 'The advanced system for security experts, developers and crypto-addicted people.';
 const CONFIG_SITE_DESCRIPTION = 'Discover our awesome cyber security GNU/Linux environment. It includes a full portable laboratory for security and digital forensics experts, but it also includes all you need to develop your own softwares or protect your privacy with anonymity and crypto tools.';
-const CONFIG_SITE_URL = 'https://www.parrot.sh/';
-const CONFIG_SITE_URL_STATIC_RESOURCES = 'https://www.parrot.sh/';
+const CONFIG_SITE_URL = 'https://www.parrotlinux.com/';
+const CONFIG_SITE_URL_STATIC_RESOURCES = 'https://www.parrotlinux.com/';
 const CONFIG_SITE_AUTOR = 'Lorenzo Faletra';
 const CONFIG_SITE_LANG = 'en';
 const CONFIG_SYSTEM_VERSION = '4.6';
--- include/layout.php
+++ include/layout.php
@@ -10,10 +10,10 @@
   <meta name="author"   content="<?php echo CONFIG_SITE_AUTOR; ?>">
   <!-- Meta data to help Facebook generate the right advertisement -->
   <meta property="og:type"        content="article" />
-  <meta property="og:title"       content="The best choice for security experts, developers and crypto-addicted people." />
-  <meta property="og:site_name"   content="Parrot Security"/>
-  <meta property="og:url"         content="https://www.parrot.sh" />
-  <meta property="og:image"       content="https://www.parrot.sh/img/facebook-preview.png" />
+  <meta property="og:title"       content="The advanced system for security experts, developers and crypto-addicted people." />
+  <meta property="og:site_name"   content="Parrot Linux"/>
+  <meta property="og:url"         content="https://www.parrotlinux.com" />
+  <meta property="og:image"       content="https://www.parrotlinux.com/img/facebook-preview.png" />
   <meta property="og:description" content="Discover our awesome cyber security GNU/Linux environment. It includes a full portable laboratory for security and digital forensics experts, but it also includes all you need to develop your own softwares or protect your privacy with anonymity and crypto tools." />
   <meta property="fb:app_id"      content="">
   <link rel="stylesheet" href="css/unified.min.css" />
@@ -82,28 +82,28 @@ th.tabco1 {
                   </ul>
                 </li>
                   <li>
-                      <a class="page-scroll" href="https://blog.parrot.sh"><?php echo $lang['nav-4']; ?></a>
+                      <a class="page-scroll" href="https://blog.parrotlinux.com"><?php echo $lang['nav-4']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://docs.parrot.sh"><?php echo $lang['nav-5']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com"><?php echo $lang['nav-5']; ?></a>
                   </li>
                   <li>
                       <a class="page-scroll" href="https://nest.parrotsec.org" target="_blank"><?php echo $lang['nav-6']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://community.parrot.sh"><?php echo $lang['nav-7']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com/community"><?php echo $lang['nav-7']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://docs.parrot.sh/partners"><?php echo $lang['nav-8']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com/partners"><?php echo $lang['nav-8']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://docs.parrot.sh/team"><?php echo $lang['nav-9']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com/team"><?php echo $lang['nav-9']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://docs.parrot.sh/donate"><?php echo $lang['nav-10']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com/donate"><?php echo $lang['nav-10']; ?></a>
                   </li>
                   <li>
-                      <a class="page-scroll" href="https://docs.parrot.sh/faq"><?php echo $lang['nav-11']; ?></a>
+                      <a class="page-scroll" href="https://docs.parrotlinux.com/faq"><?php echo $lang['nav-11']; ?></a>
                   </li>
               </ul>
               <ul class="nav navbar-nav navbar-right">
@@ -112,7 +112,7 @@ th.tabco1 {
                       <ul class="dropdown-menu language-navbar" aria-labelledby="languageDropdown">
                           <?php
                             foreach($languages as $key => $value) {
-                                $link = 'https://www' . 'parrot.sh' . $_SERVER['REQUEST_URI'];
+                                $link = 'https://www.' . 'parrotlinux.com' . $_SERVER['REQUEST_URI'];
                                 if(!strpos($link, "?lang="))
                                     $final_link = "$link?lang=$key";
                                 else {
