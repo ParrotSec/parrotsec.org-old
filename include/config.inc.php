d608f183b384e5dcabc7bb3386b28728963e9132
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2019-09-19 11:35:27 +0200

    release parrot 4.7

--- include/config.inc.php
+++ include/config.inc.php
@@ -3,13 +3,8 @@
 const CONFIG_SITE_NAME = 'Parrot OS';
 const CONFIG_SITE_SLOGAN = 'The advanced system for security experts, developers and crypto-addicted people.';
 const CONFIG_SITE_DESCRIPTION = 'Discover our awesome cyber security GNU/Linux environment. It includes a full portable laboratory for security and digital forensics experts, but it also includes all you need to develop your own softwares or protect your privacy with anonymity and crypto tools.';
-<<<<<<< HEAD
-const CONFIG_SITE_URL = 'https://www.parrotlinux.com/';
-const CONFIG_SITE_URL_STATIC_RESOURCES = 'https://www.parrotlinux.org/';
-=======
 const CONFIG_SITE_URL = 'https://parrotlinux.org/';
 const CONFIG_SITE_URL_STATIC_RESOURCES = 'https://parrotlinux.org/';
->>>>>>> ab871705d970e751d0b8e816ef36e68dae45bfd9
 const CONFIG_SITE_AUTOR = 'Lorenzo Faletra';
 const CONFIG_SITE_LANG = 'en';
 const CONFIG_SYSTEM_VERSION = '4.7';
